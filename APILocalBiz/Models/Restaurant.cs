f7fd8032621757d6b221ce9030cfeba06899cd7c
Tawnee Harris <tawneeh@icloud.com>
2021-01-22 09:36:15 -0800

    add Restaurant class

---
+++ APILocalBiz/Models/Restaurant.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+
+namespace APILocalBiz.Models
+{
+  public class Restaurant
+  {
+    public int RestaurantId { get; set; }
+    public string Name { get; set; }
+    public string Cuisine { get; set; }
+    public int Phone { get; set; }
+
+  }
+}
