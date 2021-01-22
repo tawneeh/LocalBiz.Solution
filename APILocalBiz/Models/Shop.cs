7afbd59cda5e39a3f7fc3a4de54274e834e11cc3
Tawnee Harris <tawneeh@icloud.com>
2021-01-22 09:35:51 -0800

    add Shop class

---
+++ APILocalBiz/Models/Shop.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+
+namespace APILocalBiz.Models
+{
+  public class Shop
+  {
+    public int ShopId { get; set; }
+    public string Name { get; set; }
+    public string Specialty { get; set; }
+    public int Phone { get; set; }
+
+  }
+}
