02f90921c32f788dd9aa4e8374af93d90f3b09c0
Tawnee Harris <tawneeh@icloud.com>
2021-01-22 12:15:57 -0800

    add attempt to reseed dummy data

---
+++ APILocalBiz/Migrations/20210122201341_reseedData.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace APILocalBiz.Migrations
+{
+    public partial class reseedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
