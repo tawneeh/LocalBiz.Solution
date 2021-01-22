6d15091576b0f76defb5c24d325fd6dde9392b84
Tawnee Harris <tawneeh@icloud.com>
2021-01-22 11:17:46 -0800

    add RestaurantsController.cs

--- APILocalBiz/Controllers/RestaurantsController.cs
+++ APILocalBiz/Controllers/RestaurantsController.cs
@@ -19,6 +19,59 @@ namespace APILocalBiz.Controllers
 
     // GET api/restaurants
     [HttpGet]
-    
+    public ActionResult<IEnumerable<Restaurant>> Get(string name, string phone, string cuisine, bool recommended)
+    {
+      var query = _db.Restaurants.AsQueryable();
+
+      if (name != null)
+      {
+        query = query.Where(entry => entry.Name == name);
+      }
+
+      if (cuisine != null)
+      {
+        query = query.Where(entry => entry.Cuisine == cuisine);
+      }
+
+      if (recommended == true || false)
+      {
+        query = query.Where(entry => entry.Recommended == recommended);
+      }
+
+      return query.ToList();
+    }
+
+    // POST api/restaurants
+    [HttpPost]
+    public void Post([FromBody] Restaurant restaurant)
+    {
+      _db.Restaurants.Add(restaurant);
+      _db.SaveChanges();
+    }
+
+    // GET api/restaurants/3
+    [HttpGet("{id}")]
+    public ActionResult<Restaurant> Get(int id)
+    {
+      return _db.Restaurants.FirstOrDefault(entry => entry.RestaurantId == id);
+    }
+
+    // PUT api/restaurants/3
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody] Restaurant restaurant)
+    {
+      restaurant.RestaurantId = id;
+      _db.Entry(restaurant).State = EntityState.Modified;
+      _db.SaveChanges();
+    }
+
+    // DELETE api/restaurants/3
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+      var restaurantToDelete = _db.Restaurants.FirstOrDefault(entry => entry.RestaurantId == id);
+      _db.Restaurants.Remove(restaurantToDelete);
+      _db.SaveChanges();
+    }
   }
 }
