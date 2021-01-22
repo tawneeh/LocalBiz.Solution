84b722d7983531e5c511846d05ecd8963c716848
Tawnee Harris <tawneeh@icloud.com>
2021-01-22 10:03:06 -0800

    add Initial migration

---
+++ APILocalBiz/Migrations/20210122180106_Initial.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace APILocalBiz.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Restaurants",
+                columns: table => new
+                {
+                    RestaurantId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    Phone = table.Column<string>(nullable: true),
+                    Cuisine = table.Column<string>(nullable: true),
+                    Recommended = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Restaurants", x => x.RestaurantId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Shops",
+                columns: table => new
+                {
+                    ShopId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    Phone = table.Column<string>(nullable: true),
+                    Specialty = table.Column<string>(nullable: true),
+                    Recommended = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Shops", x => x.ShopId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Restaurants",
+                columns: new[] { "RestaurantId", "Cuisine", "Name", "Phone", "Recommended" },
+                values: new object[,]
+                {
+                    { 1, "Steak", "El Gaucho", "206-728-1337", true },
+                    { 2, "Greek", "Kafe Neo", "425-375-0512", true },
+                    { 3, "Japanese", "Sushi Spott", "425-338-4553", false }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Shops",
+                columns: new[] { "ShopId", "Name", "Phone", "Recommended", "Specialty" },
+                values: new object[,]
+                {
+                    { 1, "Francesca's", "425-775-4712", true, "Women's Fashion" },
+                    { 2, "Central Market", "425-357-3240", true, "Gourmet Grocery" },
+                    { 3, "Mill Geek Comics", "425-415-6666", true, "Comic books" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Restaurants");
+
+            migrationBuilder.DropTable(
+                name: "Shops");
+        }
+    }
+}
