1af9d33bb66cf1a25771e4900cbc8ad0c8f4764c
Tawnee Harris <tawneeh@icloud.com>
2021-01-22 09:59:55 -0800

    add Seeded Data

--- APILocalBiz/Models/APILocalBizContext.cs
+++ APILocalBiz/Models/APILocalBizContext.cs
@@ -1,7 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using System.Collections;
 using System.Collections.Generic;
-using Systems.Linq;
 
 namespace APILocalBiz.Models
 {
@@ -18,11 +17,23 @@ namespace APILocalBiz.Models
     {
       builder.Entity<Shop>()
 
-          .HasData();
+          .HasData(
+              new Shop { ShopId = 1, Name = "Francesca's", Phone = "425-775-4712", Specialty = "Women's Fashion", Recommended = true },
+
+              new Shop { ShopId = 2, Name = "Central Market", Phone = "425-357-3240", Specialty = "Gourmet Grocery", Recommended = true },
+
+              new Shop { ShopId = 3, Name = "Mill Geek Comics", Phone = "425-415-6666", Specialty = "Comic books", Recommended = true }
+          );
 
       builder.Entity<Restaurant>()
 
-          .HasData();
+          .HasData(
+              new Restaurant { RestaurantId = 1, Name = "El Gaucho", Phone = "206-728-1337", Cuisine = "Steak", Recommended = true },
+
+              new Restaurant { RestaurantId = 2, Name = "Kafe Neo", Phone = "425-375-0512", Cuisine = "Greek", Recommended = true },
+
+              new Restaurant { RestaurantId = 3, Name = "Sushi Spott", Phone = "425-338-4553", Cuisine = "Japanese", Recommended = false }
+          );
     }
   }
 }
