18ed55f5e0518a7d38d59b409d8f83372d158946
Tawnee Harris <tawneeh@icloud.com>
2021-01-22 16:14:04 -0800

    add [MapToApiVersion] attribute

--- APILocalBiz/Controllers/ShopsController.cs
+++ APILocalBiz/Controllers/ShopsController.cs
@@ -8,14 +8,15 @@ using APILocalBiz.Models;
 
 namespace APILocalBiz.Controllers
 {
-  [Route("api/v{version:apiVersion}/[controller]")]
+  [Route("api/[controller]")]
   [ApiController]
   [ApiVersion("1.0")]
-  public class ShopsV1Controller : ControllerBase
+  [ApiVersion("1.1")]
+  public class ShopsController : ControllerBase
   {
     private APILocalBizContext _db;
 
-    public ShopsV1Controller(APILocalBizContext db)
+    public ShopsController(APILocalBizContext db)
     {
       _db = db;
     }
@@ -25,6 +26,31 @@ namespace APILocalBiz.Controllers
     public ActionResult<IEnumerable<Shop>> Get()
     {
       return _db.Shops.ToList();
+    }
+
+    // GET api/shops -- might be different
+    [HttpGet]
+    [MapToApiVersion("1.1")]
+    public ActionResult<IEnumerable<Shop>> Get(string name, string phone, string specialty, bool recommended)
+    {
+      var query = _db.Shops.AsQueryable();
+
+      if (name != null)
+      {
+        query = query.Where(entry => entry.Name == name);
+      }
+
+      if (specialty != null)
+      {
+        query = query.Where(entry => entry.Specialty == specialty);
+      }
+
+      if (recommended == true || false)
+      {
+        query = query.Where(entry => entry.Recommended == recommended);
+      }
+
+      return query.ToList();
     }
 
     // POST api/shops
@@ -59,57 +85,5 @@ namespace APILocalBiz.Controllers
         _db.Shops.Remove(shopToDelete);
         _db.SaveChanges();
       }
-
-    [ApiVersion("2.0")]
-    [Route("api/[controller]")]
-    [ApiController]
-    public class ShopsV2Controller : ControllerBase
-    {
-      private APILocalBizContext _db;
-
-      public ShopsV2Controller(APILocalBizContext db)
-      {
-        _db = db;
-      }
-    }
-
-    // GET api/shops
-    [HttpGet]
-    public ActionResult<IEnumerable<Shop>> GetV2(string name, string phone, string specialty, bool recommended)
-    {
-      var query = _db.Shops.AsQueryable();
-
-      if (name != null)
-      {
-        query = query.Where(entry => entry.Name == name);
-      }
-
-      if (specialty != null)
-      {
-        query = query.Where(entry => entry.Specialty == specialty);
-      }
-
-      if (recommended == true || false)
-      {
-        query = query.Where(entry => entry.Recommended == recommended);
-      }
-
-      return query.ToList();
-    }
-
-    // POST api/shops
-    [HttpPost]
-    public void PostV2([FromBody] Shop shop)
-    {
-      _db.Shops.Add(shop);
-      _db.SaveChanges();
-    }
-
-    // GET api/shops/3  
-    [HttpGet("{id}")]
-    public ActionResult<Shop> GetV2(int id)
-    {
-      return _db.Shops.FirstOrDefault(entry => entry.ShopId == id);
-    }
   }
 }
